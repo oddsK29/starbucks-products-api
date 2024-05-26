2d18df84f20d1be221da776bd0fc2582379f096f
oddsK29 <jukrin@odds.team>
2024-05-27 01:43:28 +0700

    [API] create findByGrindOption

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -1,66 +1,84 @@
+const { error } = require('console');
 const productsService = require('../services/products.services');
 
 class ProductsController {
-  async createProducts(req, reply) {
-    try {
-      const product = await productsService.createProducts(req.body);
-      reply.status(200).send(product);
-    } catch (err) {
-      req.log.error(err);
-      reply.status(500).internalServerError('Unable to create product');
+    async createProducts(req, reply) {
+        try {
+            const product = await productsService.createProducts(req.body);
+            reply.status(200).send(product);
+        } catch (err) {
+            req.log.error(err);
+            reply.status(500).internalServerError('Unable to create product');
+        }
     }
-  }
 
-  async getProductsById(req, reply) {
-    try {
-      const product = await productsService.getProductsById(req.params.id);
-      if (!product) {
-        reply.notFound(`Products with id ${req.params.id} not found`);
-      } else {
-        reply.status(200).send(product);
-      }
-    } catch (err) {
-      req.log.error(err);
-      reply.status(500).internalServerError('Unable to fetch product');
+    async getProductsById(req, reply) {
+        try {
+            const product = await productsService.getProductsById(req.params.id);
+            if (!product) {
+                reply.notFound(`Products with id ${req.params.id} not found`);
+            } else {
+                reply.status(200).send(product);
+            }
+        } catch (err) {
+            req.log.error(err);
+            reply.status(500).internalServerError('Unable to fetch product');
+        }
     }
-  }
 
-  async getAllProductss(req, reply) {
-    try {
-      const products = await productsService.getAllProductss();
-      reply.status(200).send(products);
-    } catch (err) {
-      req.log.status(500).error(err);
+    async getAllProductss(req, reply) {
+        try {
+            const products = await productsService.getAllProductss();
+            reply.status(200).send(products);
+        } catch (err) {
+            req.log.status(500).error(err);
+        }
     }
-  }
 
-  async updateProducts(req, reply) {
-    try {
-      const product = await productsService.updateProducts(req.params.id, req.body);
-      if (!product) {
-        reply.notFound(`product with id ${req.params.id} not found`);
-      } else {
-        reply.status(200).send(product);
-      }
-    } catch (err) {
-      req.log.error(err);
-      reply.status(500).internalServerError('Unable to update product');
+    async updateProducts(req, reply) {
+        try {
+            const product = await productsService.updateProducts(req.params.id, req.body);
+            if (!product) {
+                reply.notFound(`product with id ${req.params.id} not found`);
+            } else {
+                reply.status(200).send(product);
+            }
+        } catch (err) {
+            req.log.error(err);
+            reply.status(500).internalServerError('Unable to update product');
+        }
     }
-  }
 
-  async deleteProducts(req, reply) {
-    try {
-      const success = await productsService.deleteProducts(req.params.id);
-      if (!success) {
-        reply.status(404).notFound(`product with id ${req.params.id} not found`);
-      } else {
-        reply.status(200).send({ message: `product with id ${req.params.id} deleted successfully` });
-      }
-    } catch (err) {
-      req.log.error(err);
-      reply.status(500).internalServerError('Unable to delete product');
+    async findByGrindOption(req, reply) {
+        console.log('req', JSON.stringify(req.query.grind_option, null, 4));
+        const { grind_option } = req.query;
+        try {
+            if (!grind_option) {
+                throw Error()
+            }
+            const products = await productsService.getAllProductss();
+            const filterProducts = products.filter((item) => item.grind_option.includes(grind_option))
+            reply.status(200).send(filterProducts);
+        } catch (err) {
+            console.log(err);
+            req.log.error(err);
+            reply.status(500).send({ error: 'Unable to fetch products' });
+        }
     }
-  }
+
+    async deleteProducts(req, reply) {
+        try {
+            const success = await productsService.deleteProducts(req.params.id);
+            if (!success) {
+                reply.status(404).notFound(`product with id ${req.params.id} not found`);
+            } else {
+                reply.status(200).send({ message: `product with id ${req.params.id} deleted successfully` });
+            }
+        } catch (err) {
+            req.log.error(err);
+            reply.status(500).internalServerError('Unable to delete product');
+        }
+    }
 
 
 }
