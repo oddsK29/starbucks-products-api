27bc2cb19f1f506ab9b10afa156472f606529098
oddsK29 <jukrin@odds.team>
2024-05-27 11:01:50 +0700

    Update routes to get orders and orderdetails

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,10 @@
 
 const productsRoutes = require('./products.routes')
+const ordersRoutes = require('./orders.routes')
+const orderdetailRoutes = require('./orderdetail.routes')
 
 module.exports = async function (fastify, options) {
     fastify.register(productsRoutes, { prefix: '/api/products' })
+    fastify.register(ordersRoutes, { prefix: '/api/orders' })
+    fastify.register(orderdetailRoutes, { prefix: '/api/orderdateil' })
 }
