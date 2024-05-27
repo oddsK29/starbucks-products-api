4d9ae2fd83b265f156074cc130021f80e9d3068b
oddsK29 <jukrin@odds.team>
2024-05-27 16:34:16 +0700

    update format code

--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -3,14 +3,15 @@ async function ordersRoutes(fastify, options) {
     fastify.post('/', orderControllers.createOrder);
     fastify.get('/', orderControllers.getAllOrders);
     fastify.get('/:id', orderControllers.getOrderById);
-   
     fastify.put('/:id', orderControllers.updateOrder);
-    fastify.put('/status/:id', orderControllers.updateOrderStatus);
     fastify.delete('/:id', orderControllers.deleteOrder);
 
     fastify.get('/user/:users_id', orderControllers.getOrdersByUserId);
     fastify.get('/user/:id/:users_id', orderControllers.getOrderDetailsShoppingByUserId);
+    
     fastify.get('/total/:id', orderControllers.calculateTotalByOrderId);
+  
+    fastify.put('/status/:id', orderControllers.updateOrderStatus);
     fastify.put('/status/total/:id', orderControllers.completeOrderAndSumTotal);
 }
 
